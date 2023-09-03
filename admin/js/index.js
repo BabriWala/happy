9fa755c822d66c2e546fa0a4b1c8049bd15fe322
babriwala <hanzalafarhann@gmail.com>
2023-09-03 23:08:47 +0600

    dashboard analytics added

--- admin/js/index.js
+++ admin/js/index.js
@@ -13,3 +13,31 @@ $('#tabs-nav li').click(function(){
   $(activeTab).fadeIn();
   return false;
 });
+
+$(document).ready(function(){
+  const analyticsChart = document.getElementById('analyticsChart')
+console.log(analyticsChart)
+const data = {
+ 
+  datasets: [{
+    label: 'My First Dataset',
+    data: [100, 50, 100],
+    backgroundColor: [
+      'rgb(255, 99, 132)',
+      'rgb(54, 162, 235)',
+      'rgb(255, 205, 86)'
+    ],
+    hoverOffset: 4
+  }]
+};
+
+new Chart(analyticsChart,{
+  type: 'doughnut',
+  data: data,
+  options: {
+    cutout: 110,
+    cutoutPercentage: 40,
+  }
+})
+});
+
