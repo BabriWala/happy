f97d644cd42de14a38932d51e53d5583911bcdef
babriwala <hanzalafarhann@gmail.com>
2023-09-04 14:07:21 +0600

    SR Profile Modal Done

--- so/js/index.js
+++ so/js/index.js
@@ -11,4 +11,20 @@ $('.js-btn-modal').on('click', function(){
     $('#overlay').fadeOut();
     $('.js-modal').fadeOut();
   });
+
+
+
+$('.js-btn-profile-modal').on('click', function(){
+    $('#overlay').fadeIn();
+    $('.js-profile-modal').fadeIn();
+  });
+  
+  $('.js-profile-close-btn').on('click', function(){
+    $('#overlay').fadeOut();
+    $('.js-profile-modal').fadeOut();
+  });
+  $('#overlay').on('click', function(){
+    $('#overlay').fadeOut();
+    $('.js-profile-modal').fadeOut();
+  });
   
